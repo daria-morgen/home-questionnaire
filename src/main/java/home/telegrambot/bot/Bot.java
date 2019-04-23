f315a8f960935410c87a2c1ba8e362ac5985a62f
daria <dk.957@ya.ru>
2019-04-23 12:48:05 +0300

    git init

---
+++ src/main/java/home/telegrambot/bot/Bot.java
@@ -0,0 +1,65 @@
+package home.telegrambot.bot;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    private String botUsername;
+
+    private String botToken;
+
+    /**
+     * Метод для приема сообщений.
+     * @param update Содержит сообщение от пользователя.
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    /**
+     * Метод для настройки сообщения и его отправки.
+     * @param chatId id чата
+     * @param s Строка, которую необходимот отправить в качестве сообщения.
+     */
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+//            log.log(Level.SEVERE, "Exception: ", e.toString());
+        }
+    }
+
+    /**
+     * Метод возвращает имя бота, указанное при регистрации.
+     * @return имя бота
+     */
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    /**
+     * Метод возвращает token бота для связи с сервером Telegram
+     * @return token для бота
+     */
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    public static Bot getBot(String botUsername, String botToken){
+        Bot bot = new Bot();
+        bot.botUsername=botUsername;
+        bot.botToken=botToken;
+        return bot;
+    }
+}
---
+++ src/main/java/home/telegrambot/config/BotConfig.java
@@ -0,0 +1,32 @@
+package home.telegrambot.config;
+
+import home.telegrambot.bot.Bot;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+@Configuration
+public class BotConfig {
+
+    @Value("${botSource.botUserName}")
+    private String botUsername;
+
+    @Value("${botSource.botToken}")
+    private String botToken;
+
+    @Bean
+    public void configureBotSource() {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+
+        try {
+            telegramBotsApi.registerBot(Bot.getBot(botUsername,botToken));
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
