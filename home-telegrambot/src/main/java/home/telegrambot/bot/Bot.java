bcbd4b9c04d8fdee371b7dcf0c3892742c772c06
daria <dk.957@ya.ru>
2019-05-10 17:00:42 +0300

    think about buttons

--- home-telegrambot/src/main/java/home/telegrambot/bot/Bot.java
+++ home-telegrambot/src/main/java/home/telegrambot/bot/Bot.java
@@ -2,9 +2,17 @@ package home.telegrambot.bot;
 
 import home.telegrambot.service.PersistenceService;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.StrictMath.toIntExact;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -20,84 +28,39 @@ public class Bot extends TelegramLongPollingBot {
      */
     @Override
     public void onUpdateReceived(Update update) {
-        String message = update.getMessage().getText();
-        sendMsg(update.getMessage().getChatId().toString(), message);
+//        String message = update.getMessage().getText();
+//        sendMsg(update.getMessage().getChatId().toString(), message);
 
 
-//
-//        if(update.hasMessage()) {
-//            ThreadClass thread = new ThreadClass(update.getMessage());
-//        } else  if(update.hasCallbackQuery()) {
-//            AnswerCallbackThread answerThread = new AnswerCallbackThread(update.getCallbackQuery());
-//        }
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            if (update.getMessage().getText().equals("/start")) {
 
-//        if (update.hasMessage() && update.getMessage().hasText()) {
-//            String message_text = update.getMessage().getText();
-//            long chat_id = update.getMessage().getChatId();
-//            if (update.getMessage().getText().equals("/start")) {
-//
-//
-//                SendMessage message = new SendMessage() // Create a message object object
-//                        .setChatId(chat_id)
-//                        .setText("You send /start");
-//                InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
-//                List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
-//                List<InlineKeyboardButton> rowInline = new ArrayList<>();
-//                rowInline.add(new InlineKeyboardButton().setText("Update message text").setCallbackData("update_msg_text"));
-//                // Set the keyboard to the markup
-//                rowsInline.add(rowInline);
-//                // Add it to the message
-//                markupInline.setKeyboard(rowsInline);
-//                message.setReplyMarkup(markupInline);
-//                try {
-//                    execute(message); // Sending our message object to user
-//                } catch (TelegramApiException e) {
-//                    e.printStackTrace();
-//                }
-//            } else {
-//
-//            }
-//
-//        } else if (update.hasCallbackQuery()) {
-//            // Set variables
-//            String call_data = update.getCallbackQuery().getData();
-//            long message_id = update.getCallbackQuery().getMessage().getMessageId();
-//            long chat_id = update.getCallbackQuery().getMessage().getChatId();
-//
-//            if (call_data.equals("update_msg_text")) {
-//                String answer = "Updated message text";
-//                EditMessageText new_message = new EditMessageText()
-//                        .setChatId(chat_id)
-//                        .setMessageId(toIntExact(message_id))
-//                        .setText(answer);
-//                try {
-//                    execute(new_message);
-//                } catch (TelegramApiException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//        }
+                SendMessage message = InlineKeyboardBuilder.create(update.getMessage().getChatId())
+                        .setText("Menu:")
+                        .row()
+                        .button("Save my word", "call")
+                        .button("Start test", "in dev")
+                        .endRow()
+                        .build();
+                try {
+                    execute(message); // Sending our message object to user
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else {
 
-
-//        SendMessage message = InlineKeyboardBuilder.create(update.getMessage().getChatId())
-//                .setText("Menu:")
-//                .row()
-//                .button("Action 1", "action-1")
-//                .button("Action 2", "action-2")
-//                .endRow()
-//                .row()
-//                .button("Action 3", "action-3")
-//                .endRow()
-//                .build();
-//
-//        try {
-//            // Send the message
-//            sendApiMethod(message);
-//            //execute(message);
-//        } catch (TelegramApiException e) {
-//            e.printStackTrace();
-//        }
+            }
+        } else if (update.hasCallbackQuery()) {
+           EditMessageText new_message = new EditMessageText()
+                        .setChatId(update.getCallbackQuery().getMessage().getChatId())
+                        .setMessageId(toIntExact(update.getCallbackQuery().getMessage().getMessageId()))
+                        .setText("success");
+            try {
+                execute(new_message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
